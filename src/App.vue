2e34d3abb3498f62291ec5134ef958bb2dd0f23c
Thomas CILES <tciles@sapiens.biz>
2017-11-30 08:29:18 +0100

    create api

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="app">
-    <DatePicker/>
-  </div>
+    <div id="app">
+        <DatePicker/>
+        {{ now }}
+    </div>
 </template>
 
 <script>
-import DatePicker from './components/DatePicker.vue'
+    import DatePicker from './components/DatePicker.vue'
+    import dateApi from './api/date';
 
-export default {
-  name: 'app',
-  components: {
-      DatePicker
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            DatePicker
+        },
+        computed: {
+            ...dateApi
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/api/date.js
@@ -0,0 +1,11 @@
+import * as moment from 'moment';
+
+const now = () => moment.now();
+
+
+
+
+
+export default {
+    now
+};
